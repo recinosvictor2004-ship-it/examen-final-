f55a3cb84da12936d2e41749bacc903f7d67c44d
recinosvictor2004 <recinosvictor2004@gmail.com>
2026-08-31 16:52:24 -0600

    feat: :sparkles: ingresos generados

--- DQL.sql
+++ DQL.sql
@@ -4,3 +4,10 @@ JOIN Producto p ON d.id_producto = p.id_producto
 JOIN CategoriaProducto c ON p.id_categoria = c.id_categoria
 GROUP BY p.id_producto
 ORDER BY total_vendido DESC;
+
+
+SELECT co.nombre, SUM(d.cantidad * d.precio_unitario) AS ingresos_combo
+FROM PedidoDetalle d
+JOIN Combo co ON d.id_combo = co.id_combo
+WHERE d.id_combo IS NOT NULL
+GROUP BY co.id_combo;
